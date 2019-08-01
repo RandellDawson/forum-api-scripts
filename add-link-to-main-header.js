6a160d609604a0f8396b54478a624f03499bc5ce
Randell Dawson <rdawson@onepathtech.com>
2019-08-01 00:01:27 -0700

    feat: function to add backlink challenge

--- add-link-to-main-header.js
+++ add-link-to-main-header.js
@@ -10,25 +10,4 @@ const convertMainHeaderToLink = ({ guideArticleContent, challengeFilePath }) => 
 
 };
 
-const guideArticleContent = `
-# Match Non-Whitespace Characters
-
-
----
-## Hints
-
-### Hint 1
-
-* A global flag will help you get through this challenge.
-
-### Hint 2
-
-* Try using a shorthand character for S non-whitespace.
-
-
-`;
-const challengeFilePath = "curriculum/challenges/english/02-javascript-algorithms-and-data-structures/regular-expressions/match-non-whitespace-characters.english.md";
-
-console.log(convertMainHeaderToLink({ guideArticleContent, challengeFilePath }));
-
 module.exports = { convertMainHeaderToLink };
